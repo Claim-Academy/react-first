8849652421b089061f978c17db349cad8ef67a75
Manav Misra <manavm@visionwebsoft.com>
2023-04-14 16:03:34 -0500

    Make price as number types

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Products from "./components/products";
 import SelectCategories from "./components/select-categories";
 
 const data = [
-  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+  { category: "Fruits", price: 1, stocked: true, name: "Apple" },
+  { category: "Fruits", price: 1, stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: 2, stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: 2, stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: 4, stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: 1, stocked: true, name: "Peas" },
 ];
 
 const uniqueCategories = [...new Set(data.map((item) => item.category))];
